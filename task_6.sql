d1a67a0eb4ac9ad04613526389ac8558664842a4
Ali Abdelhakam <aliabdo96@gmail.com>
2024-10-19 23:30:50 +0300

    question 7 modified x2

--- task_6.sql
+++ task_6.sql
@@ -4,11 +4,11 @@ USE alx_book_store;
 
 -- Insert multiple rows into the Customers table with individual INSERT statements
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.');
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
