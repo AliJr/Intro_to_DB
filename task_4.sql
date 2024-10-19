d6de99d88c4d0a89d3c3d75a579265c1f788a4ff
Ali Abdelhakam <aliabdo96@gmail.com>
2024-10-19 23:17:56 +0300

    question 5 modified

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,6 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Print the full description of the Books table without using DESCRIBE or EXPLAIN
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'Books';
