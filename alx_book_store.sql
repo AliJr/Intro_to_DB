c9449e51a36187cc5af2d3f19b0160c0143a11c8
Ali Abdelhakam <aliabdo96@gmail.com>
2024-10-19 23:02:31 +0300

    question 0 modify again

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -35,8 +35,7 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id)
-        REFERENCES Customers (customer_id)
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create the Order_Details table with the correct foreign keys
